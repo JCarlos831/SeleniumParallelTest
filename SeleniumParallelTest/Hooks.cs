aee878c7b62ebab27707babe8c5412d3ceb28ab7
Juan Montoya <A-JMontoya@jewelersboard.local>
2018-07-18 16:20:02 -0400

    Add project files.

---
+++ SeleniumParallelTest/Hooks.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium.Firefox;
+
+namespace SeleniumParallelTest
+{
+    public class Hooks : Base
+    {
+        public Hooks()
+        {
+            Driver = new FirefoxDriver();
+        }
+    }
+}
---
+++ SeleniumParallelTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumParallelTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumParallelTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a5fec87f-ec50-4d2c-92b7-908191259ad2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
